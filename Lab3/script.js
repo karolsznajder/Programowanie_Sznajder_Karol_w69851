d7fe53c95bbf0cbe6693a799e30382db9fc5fe3b
karolsznajder <karolsznajder@icloud.com>
2024-04-30 20:39:48 +0200

    create task no. 3 and no. 4

--- Lab3/script.js
+++ Lab3/script.js
@@ -22,3 +22,28 @@ document.getElementById("dodawanie").innerHTML = "Wynik dodawania: " + dodawanie
 document.getElementById("odejmowanie").innerHTML = "Wynik odejmowania:" + odejmowanie
 document.getElementById("mnozenie").innerHTML = "Wynik mnożenia:" + mnozenie
 document.getElementById("dzielenie").innerHTML = "Wynik dzielenia: " + dzielenie
+
+// Zadanie 4
+
+for (var i=0; i <= 100; i++)
+    if(i % 2 == 0) {
+        console.log(i);
+    }
+
+// Zadanie 5
+
+function obliczPoleTrojkata(a, b, c) {
+    var p = (a + b + c) / 2;
+    var pole = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+    return pole;
+}
+
+var a = 5;
+var b = 6;
+var c = 7;
+
+var pole = obliczPoleTrojkata(a, b, c);
+console.log("Pole trójkąta o bokach " + a + ", " + b + ", " + c + " wynosi: " + pole);
+
+document.getElementById("wynik").innerHTML = "Pole trójkąta o bokach " + a + ", " + b + ", " + c + " wynosi: " + pole;
