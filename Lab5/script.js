503dcd35724adff61c23c1b029d9a95176113bf3
Karol <karolsznajder660@gmail.com>
2024-05-12 20:21:45 +0200

    last tasks

--- Lab5/script.js
+++ Lab5/script.js
@@ -25,3 +25,56 @@ console.log("Zaktualizowana tablica:", number);
 let ciag = prompt("Wpisz ciąg znaków do odwrócenia:");
 let odwroconyCiąg = ciag.split('').reverse().join('');
 console.log("Odwrocony ciąg:", odwroconyCiąg);
+
+// Zadanie 5
+
+let numbers = [10, 20, 30, 40, 50];
+console.log("Suma:", numbers.reduce((a, b) => a + b, 0));
+console.log("Parzyste numbers:", numbers.filter(x => x % 2 === 0));
+console.log("Pomnożone wartości:", numbers.map(x => x * 3));
+numbers.push(69851);
+console.log("Index numeru albumu:", numbers.indexOf(69851));
+console.log("Średnia arytmetyczna:", numbers.reduce((a, b) => a + b, 0) / numbers.length);
+console.log("Największa liczba:", Math.max(...numbers));
+console.log("Ilość wystąpień wartości 20:", numbers.filter(x => x === 20).length);
+
+// Zadanie 6
+
+function generateFibonacci(n) {
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+  }
+  console.log(generateFibonacci(100));
+  
+// Zadanie 7
+
+function sumOfTwoLargest(numbers) {
+    let sorted = numbers.sort((a, b) => b - a);
+    return sorted[0] + sorted[1];
+  }
+  let number1 = [10, 20, 30, 40, 50];
+  console.log("Suma dwóch największych liczb:", sumOfTwoLargest(number1));
+  
+// Zadanie 8
+
+function removeDuplicates(array) {
+    return [...new Set(array)];
+  }
+  let number2 = [1, 2, 2, 3, 4, 5, 5, 6];
+  console.log("Tablica bez duplikatów:", removeDuplicates(number2));
+  
+// Zadanie 9
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+  let liczba = 17;
+  console.log(liczba + " jest liczbą pierwszą?", isPrime(liczba));
+  
